1d7c017b675aa50d1b3eeb65403f2bffecb122a3
jacob <jacoblam87@gmail.com>
2024-01-09 21:31:55 -0500

    Editing the initial code in order to try and encrypt and decrypt messages

--- code/caesar.py
+++ code/caesar.py
@@ -1,29 +1,43 @@
 import sys
 
 def encrypt(message, k):
+    alphalist = []
     for l in message:
         n = ord(l)
         numr = n + k 
         if numr > 123:
             barrier = numr - 123 
             newnumr = 96 + barrier
-    alpha = chr(n)
-    return alpha
-
+            alpha = chr(newnumr)
+            alphalist.append(alpha)
+        else:
+            alpha = chr(numr)
+            alphalist.append(alpha)
+    return alphalist
 
 def decrypt(message, k):
+    alphalist = []
     for l in message:
         n = ord(l)
         numr = n - k
-        if numr  < 
-    return alpha
+        if numr < 93:
+            barrier = 93 - numr
+            newnumr = 123 - barrier
+            alpha = chr(newnumr)
+            alphalist.append(alpha)
+        else:
+            alpha = chr(numr)
+            alphalist.append(alpha)
+    return alphalist
 
 
 if __name__ == "__main__":
     # take in first arg as word
-    message = sys.argv[1]
+    #message = sys.argv[1]
     # take in second arg as int key
-    key = int(sys.argv[2])
+    #key = int(sys.argv[2])
+    message = "testing"
+    key = 10
 
     # encrypt your word
     encrypted = encrypt(message, key)
