2f64c5b387dedb297852c2e547ffcd2f3619a856
jacob <jacoblam87@gmail.com>
2024-01-26 21:14:27 +0800

    Finalizing the code with complete encoding and decoding functionalities. Updating the read me with relevant information about the project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # caesar-cipher-lab
 Encrypting messages with Caesar's Cipher
+
+In this lab I had to program a code that will help to encode and decode a message when providing a word and a numeric key
+
+The most challenging part of this lab was figuring out how to design a barrier which will keep the ASCII numbers
+within the range of the letters that I wish to encode. In my case it was lowercase a-z which was ASCII numbers 97 - 123. I created this barrier using a for loop with a built in if and else statement. 
+
+The principal of the code was programmed. In the future if I wish to take this project further I could also add in more ASCII values, special characters and maybe even different languages. Overall this lab was very enjoyable and fun!
--- code/caesar.py
+++ code/caesar.py
@@ -1,13 +1,18 @@
 import sys
 
 def encrypt(message, k):
+# Creating a list to store our encoded message
     alphalist = []
     for l in message:
         n = ord(l)
         numr = n + k 
+# Using if and else statement in order to build a barrier for our code that allows us to encode
+# and decode within the range from 97 through 123 no matter which K value is provided 
+# which represents lowercase alphabets from a - z
         if numr > 123:
-            barrier = numr - 123 
-            newnumr = 96 + barrier
+# Dividing by the remainder will ensure that we will never exceed the limits set by our barrier
+            barrier = (numr - 123) % 26
+            newnumr = 97 + barrier
             alpha = chr(newnumr)
             alphalist.append(alpha)
         else:
@@ -20,8 +25,8 @@ def decrypt(message, k):
     for l in message:
         n = ord(l)
         numr = n - k
-        if numr < 93:
-            barrier = 93 - numr
+        if numr < 97:
+            barrier = (97 - numr) % 26
             newnumr = 123 - barrier
             alpha = chr(newnumr)
             alphalist.append(alpha)
@@ -33,11 +38,9 @@ def decrypt(message, k):
 
 if __name__ == "__main__":
     # take in first arg as word
-    #message = sys.argv[1]
+    message = sys.argv[1]
     # take in second arg as int key
-    #key = int(sys.argv[2])
-    message = "testing"
-    key = 10
+    key = int(sys.argv[2])
 
     # encrypt your word
     encrypted = encrypt(message, key)
